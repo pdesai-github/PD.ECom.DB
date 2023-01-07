51981605a76f956cab2440558f8420046a38a550
Pradipkumar Desai <pdesai.programming@gmail.com>
2023-01-07 19:51:00 +0530

    First commit

---
+++ PD.ECom.DB/Tables/Catagory.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE [Product].[Catagory]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+ CONSTRAINT [Pk_Catagory] PRIMARY KEY ([Id]),
+ CONSTRAINT [Ak_Catagory] UNIQUE NONCLUSTERED([Name])
+ )
+
